049c51204c63ac20261d0e11fc1d8eeaaa20c506
TaichiSato-starcraft <135800501+TaichiSato-starcraft@users.noreply.github.com>
2024-01-30 17:59:40 +0900

    Add files via upload

---
+++ ui-practice-git/dropdown/script.js
@@ -0,0 +1,38 @@
+const ham = $('#js-hamburger');
+const nav = $('#js-nav');
+ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+  ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+  nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外
+});
+$(window).on('scroll',function(){
+  var scrollDistance = $(this).scrollTop();
+  var $header = $('header');
+  if(scrollDistance > 500){
+    $header.addClass('fade-out');
+  } else {
+    $header.removeClass('fade-out');
+  }
+})
+const $pageTopButton = $('.page-top-button'); 
+
+$(window).on('scroll', function() {
+
+  var scrollDistance = $(this).scrollTop();
+
+  if(scrollDistance > 500) {
+    $pageTopButton.addClass('active');
+    $pageTopButton.removeClass('non-active');
+  } else {
+    $pageTopButton.addClass('non-active');
+    $pageTopButton.removeClass('active');
+  }
+
+});
+
+$('.page-top-button__link').click(function() {
+  $('body, html').animate({
+    scrollTop: 0
+  }, 500);
+  return false;
+});
+
